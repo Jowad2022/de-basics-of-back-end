196a75058423629532b0fcbad1aaf276ea476acd
Jowad2022 <99059303@mydavinci.nl>
2023-05-30 10:26:06 +0200

    commit

--- Opdrachten-PHP/welcome/test.php
+++ Opdrachten-PHP/welcome/test.php
@@ -9,14 +9,9 @@
 </head>
 
 <head>
-    <body class="BG" id="centering">
-        <h1 class ="text"> welcome</h1>
-        <form action="welcome.php" method="POST">
-        Name: <input type="text" name="name"><br>
-        E-mail: <input type="text" name="email"><br>
-        <input type="Submit">
-        </form>
+    <body class="BG" id="centering">   
         <?php 
+            $nameErr = $emailErr = "";
             $name = $email = "";
 
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -38,6 +33,22 @@
                 $data = htmlspecialchars($data);
                 return $data;
             }
+        ?>
+        <h1 class ="text"> welcome</h1>
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+            Name: <input type="text" name="name">
+            <span class="error">* <?php echo $nameErr;?></span>
+            <br><br>
+            E-mail: <input type="text" name="email">
+            <span class="error">* <?php echo $emailErr;?></span>
+            <br><br>
+            <input type="submit" name="submit" value="Submit">  
+            </form>
+        <?php
+        echo "<h2>Your Input:</h2>";
+        echo "Naam: ",$name;
+        echo "<br>";
+        echo "E-mailadres: ",$email;
         ?>
     </body>
 </head>
---
+++ Opdrachten-PHP/welcome/welcome.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h1 class ="text"> De ingevulde gegevens zijn:</h1>
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+</body>
+</html>
